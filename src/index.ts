d85a659eef3c665b18cd4b87e6de05422c063385
Joseph Aulner <joseph.aulner@aviture.us.com>
2018-04-16 15:00:17 -0500

    Clean up href selection & use async/await to open each link

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,40 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const nightmare = new Nightmare({
-    loadImages: false,
     show: true,
     openDevTools: {
         mode: 'detach'
     }
 });
 
-nightmare
-    .goto('https://account.microsoft.com/rewards/')
-    .click('#signinhero')
-    .type('input[name="loginfmt"]', process.env.MS_USER)
-    .click('input[value="Next"]')
-    .wait(() => !document.querySelector('input[name="passwd"]').classList.contains('moveOffScreen'))
-    .type('input[name="passwd"]', process.env.MS_PASS)
-    .click('input[Value="Sign in"]')
-    .wait('mee-rewards-daily-set-section')
-    .evaluate(() => {
-        // todo: figure out why element list is being "sanitized" before being returned to the node execution context
-        const dailyCards: Element[] = [];
-        const elements = document.querySelectorAll('mee-rewards-daily-set-section button + mee-card-group mee-card').values();
-        let next = elements.next();
-        while (!next.done) {
-            console.log(next.value);
-            dailyCards.push(next.value);
-            next = elements.next();
+(async () => {
+    try {
+        const hrefs = await getDailyLinks();
+        for (let href of hrefs) {
+            await nightmare.goto(href).wait(5000).then(_ => _);
+            // todo: add logic to handle quizes and such :)
         }
-        return dailyCards;
-    })
-    .then((data) => {
-        debugger;
-    })
-    .catch((err) => {
-        debugger;
-    });
+        nightmare.end().then(_ => _);
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+async function getDailyLinks() {
+    return nightmare
+        .goto('https://account.microsoft.com/rewards/')
+        .click('#signinhero')
+        .type('input[name="loginfmt"]', process.env.MS_USER)
+        .click('input[value="Next"]')
+        .wait(() => !document.querySelector('input[name="passwd"]').classList.contains('moveOffScreen'))
+        .type('input[name="passwd"]', process.env.MS_PASS)
+        .click('input[Value="Sign in"]')
+        .wait('mee-rewards-daily-set-section')
+        .evaluate(() => {
+            const iterator = document.querySelectorAll('mee-rewards-daily-set-section button + mee-card-group mee-card').values();
+            return Array.from(iterator)
+                .map((element) => element.querySelector('a.c-call-to-action'))
+                .map((anchor: HTMLAnchorElement) => anchor.href);
+        })
+        .then((hrefs: string[]) => hrefs);
+}
