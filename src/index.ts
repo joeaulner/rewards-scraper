2a4d3a601dc7ec9073b4c4f06c79dbc5d24f04cd
Joseph Aulner <joseph.aulner@aviture.us.com>
2018-04-16 11:39:05 -0500

    Wait & select daily cards on sign-in; use env variables for credentials Also waits for pasword input to be visible before beginning keystrokes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import * as Nightmare from 'nightmare';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const nightmare = new Nightmare({
     loadImages: false,
-    show: true
+    show: true,
+    openDevTools: {
+        mode: 'detach'
+    }
 });
 
 nightmare
     .goto('https://account.microsoft.com/rewards/')
     .click('#signinhero')
-    .type('input[name="loginfmt"]', '*****')
+    .type('input[name="loginfmt"]', process.env.MS_USER)
     .click('input[value="Next"]')
-    .type('input[name="passwd"]', '*****')
+    .wait(() => !document.querySelector('input[name="passwd"]').classList.contains('moveOffScreen'))
+    .type('input[name="passwd"]', process.env.MS_PASS)
     .click('input[Value="Sign in"]')
+    .wait('mee-rewards-daily-set-section')
+    .evaluate(() => {
+        // todo: figure out why element list is being "sanitized" before being returned to the node execution context
+        const dailyCards: Element[] = [];
+        const elements = document.querySelectorAll('mee-rewards-daily-set-section button + mee-card-group mee-card').values();
+        let next = elements.next();
+        while (!next.done) {
+            console.log(next.value);
+            dailyCards.push(next.value);
+            next = elements.next();
+        }
+        return dailyCards;
+    })
     .then((data) => {
         debugger;
     })
